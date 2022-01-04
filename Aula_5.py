58dbd9a1b51187b75e1979e233a194350c3d856f
Jonathas Martins da Rocha <84166332+JhonAI13@users.noreply.github.com>
2022-01-04 16:41:07 -0300

    Create Aula_5.py

---
+++ Aula_5.py
@@ -0,0 +1,4 @@
+#Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor
+num = int(input('Qual numero?:'))
+print('O sucessor de {} é {}'.format(num, num + 1))
+print('O antecessor de {} é {}'.format(num, num - 1))
