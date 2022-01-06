fa9718fb76ab2628d88b674c26f25e0b2c1b6c25
Jonathas Martins da Rocha <84166332+JhonAI13@users.noreply.github.com>
2022-01-05 21:20:12 -0300

    Aulas 7 e 8

---
+++ Aula_7.py
@@ -0,0 +1,5 @@
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+n = int(input('Qual sua primeira nota?:'))
+n2 = int(input('Qual sua segunda nota?:'))
+M = (n + n2) / 2
+print('A sua media é:', M)
---
+++ Aula_8.py
@@ -0,0 +1,4 @@
+# Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milímetros.
+n = int(input('Numero em metros:'))
+print('Em centimetros:', n * 100)
+print('Em milimetros:', n * 1000)
