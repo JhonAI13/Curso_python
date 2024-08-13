ed485bf505847b2a6906faf189fc56fe08c38fdd
Jonathas Martins da Rocha <84166332+JhonAI13@users.noreply.github.com>
2024-08-13 00:12:24 -0300

    poo

---
+++ Orientação-a-objetos-jupter/Caderno/.ipynb_checkpoints/teste_conta-checkpoint.py
@@ -0,0 +1,46 @@
+class Conta:
+    def	__init__(self, numero, titular, saldo, limite):
+        """cria uma conta
+
+        Args:
+        numero (_type_): numero de identificação da conta
+        titular (_type_): nome do titular
+        saldo (_type_): quantidade de dinhiro que tem na conta
+        limite (_type_): _description_
+        """ 
+        print("inicializando uma conta")
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+        pass
+
+
+    def	deposita(self,	valor):
+        """Soma um valor a conta
+
+        Args:
+            conta (_type_): numero da conta
+            valor (_type_): quantidade a ser somada a conta
+        """
+        self.saldo	+=	valor
+
+    def	saca(self,	valor):
+        """subtrai o valor da conta.
+
+        Args:
+            conta (_type_): A conta cadastrada
+            valor (_type_): O valor a ser sacado
+        """
+        self.saldo	-=	valor
+
+    def	extrato(self):
+        """Dis quanto tem na conta
+
+        Args:
+            conta (_type_): numero da conta
+        """
+        print("numero: {} \nsaldo: {}".format(self.numero, self.saldo))
+
+    def teste(self)
+        return self
--- Orientação-a-objetos-jupter/Caderno/teste.py
+++ Orientação-a-objetos-jupter/Caderno/teste.py
@@ -1,13 +1,38 @@
 def	cria_conta(numero,	titular,	saldo,	limite):
-    conta	=	{"numero":	numero,	"titular":	titular,	"saldo":	saldo,	"limite":	limite}
-    return	conta
+	"""cria uma conta
 
+	Args:
+		numero (_type_): numero de identificação da conta
+		titular (_type_): nome do titular
+		saldo (_type_): quantidade de dinhiro que tem na conta
+		limite (_type_): _description_
+	"""
+	conta	=	{"numero":	numero,	"titular":	titular,	"saldo":	saldo,	"limite":	limite}
+	return	conta
 
 def	deposita(conta,	valor):
-    conta['saldo']	+=	valor
-    
+	"""Soma um valor a conta
+
+	Args:
+		conta (_type_): numero da conta
+		valor (_type_): quantidade a ser somada a conta
+	"""
+	conta['saldo']	+=	valor
+
 def	saca(conta,	valor):
-    conta['saldo']	-=	valor
-    
+	"""subtrai o valor da conta.
+
+	Args:
+		conta (_type_): A conta cadastrada
+		valor (_type_): O valor a ser sacado
+	"""
+	conta['saldo']	-=	valor
+
 def	extrato(conta):
-    print("numero: {} \nsaldo: {}".format(conta['numero'],	conta['saldo']))
+	"""Dis quanto tem na conta
+
+	Args:
+		conta (_type_): numero da conta
+	"""
+	print("numero: {} \nsaldo: {}".format(conta['numero'],	conta['saldo']))
+help(saca)
